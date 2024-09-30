c82ab3833a07e5113a1c68445b3e23bc19740e18
G <xandir1993@gmail.com>
2024-09-30 12:41:58 +0300

    Revert "Списки 6.4 Часть 4"
    This reverts commit 825d24b708c480b50b6c5883a88ff94eac838721.

--- Stepik/Program.cs
+++ Stepik/Program.cs
@@ -1,22 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
+using System.Linq;
 
 public class MainClass
 {
     public static void Main(string[] args)
     {
-        List<string> data = ReadInput();
-        var result = data.Where(x => x.StartsWith("image"));
-
-        Console.WriteLine(JsonSerializer.Serialize(result));
+        List<int> data = ReadInput();
+        string result = Balance(data);
+        Console.WriteLine(result);
     }
 
+    public static string Balance(List<int> data)
+    {
+        int center = data.Count / 2;
+        int left = 0;
+        int right = 0;
+        string result = "";
 
-    public static List<string> ReadInput()
+        for (int i = 0; i < center; i++)
+            left += data[i];
+
+        for (int i = center; i < data.Count; i++)
+            right += data[i];
+
+        if (left > right)
+            result = "Левая сторона тяжелее";
+        else if (right > left)
+            result = "Правая сторона тяжелее";
+        else
+            result = "Обе стороны сбалансированы";
+
+        return result;
+    }
+
+    public static List<int> ReadInput()
     {
         string input = Console.ReadLine();
-        return JsonSerializer.Deserialize<List<string>>(input.Trim());
+        return JsonSerializer.Deserialize<List<int>>(input.Trim());
     }
 }
