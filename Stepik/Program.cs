ada78078338cce904cd613acfcfc6ca5021ec002
G <xandir1993@gmail.com>
2024-09-30 12:42:53 +0300

    Списки 6.4 Часть 4 Задание 3

--- Stepik/Program.cs
+++ Stepik/Program.cs
@@ -1,43 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.Json;
 using System.Linq;
+using System.Text.Json;
 
 public class MainClass
 {
     public static void Main(string[] args)
     {
-        List<int> data = ReadInput();
-        string result = Balance(data);
-        Console.WriteLine(result);
+        List<string> data = ReadInput();
+        var result = data.Where(x => x.StartsWith("image"));
+
+        Console.WriteLine(JsonSerializer.Serialize(result));
     }
 
-    public static string Balance(List<int> data)
-    {
-        int center = data.Count / 2;
-        int left = 0;
-        int right = 0;
-        string result = "";
 
-        for (int i = 0; i < center; i++)
-            left += data[i];
-
-        for (int i = center; i < data.Count; i++)
-            right += data[i];
-
-        if (left > right)
-            result = "Левая сторона тяжелее";
-        else if (right > left)
-            result = "Правая сторона тяжелее";
-        else
-            result = "Обе стороны сбалансированы";
-
-        return result;
-    }
-
-    public static List<int> ReadInput()
+    public static List<string> ReadInput()
     {
         string input = Console.ReadLine();
-        return JsonSerializer.Deserialize<List<int>>(input.Trim());
+        return JsonSerializer.Deserialize<List<string>>(input.Trim());
     }
 }
