53c9e05b7612c61863df3ce89932bbdcf5740405
G <xandir1993@gmail.com>
2024-10-09 10:52:13 +0300

    Напишите код, который принимает список целых чисел data и записывает в переменную result третье уникальное максимальное число в этом списке. Если третье уникальное максимальное число не существует, то ваш код должен записать в переменную result максимальное число из списка.

--- Stepik/Program.cs
+++ Stepik/Program.cs
@@ -7,28 +7,14 @@ public class MainClass
 {
     public static void Main(string[] args)
     {
-        List<double> data = ReadInput();
-        string result = CountNumbers(data);
+        List<int> data = ReadInput();
+        int result = data.Distinct().Last();
         Console.WriteLine(result);
     }
 
-    public static string CountNumbers(List<double> data)
-    {
-        int uniqueCount = data.GroupBy(n => n)
-                                  .Where(g => g.Count() == 1)
-                                  .Select(g => new { Value = g.Key }).Count(); ;
-        int duplicateCount = data.GroupBy(n => n)
-                                  .Where(g => g.Count() > 1)
-                                  .Select(g => new { Value = g.Key }).Count();
-        int zeroCount = data.Count(x => x == 0);
-        int evenCount = data.Count(x => x % 2 == 0 && x != 0);
-        int oddCount = data.Count(x => x % 2 != 0);
-        return string.Join(", ", uniqueCount, duplicateCount, zeroCount, evenCount, oddCount);
-    }
-
-    public static List<double> ReadInput()
+    public static List<int> ReadInput()
     {
         string input = Console.ReadLine();
-        return JsonSerializer.Deserialize<List<double>>(input);
+        return JsonSerializer.Deserialize<List<int>>(input);
     }
 }
