cf0f1c02db2363354160f539114d89c2559dea2f
G <xandir1993@gmail.com>
2024-10-10 10:30:38 +0300

    data - список из элементов целых числел. Элементы 0 и одна 1.
    Напишите код, который определяет количество 0 до и после 1 и записывает результат в переменную result.

--- Stepik/Program.cs
+++ Stepik/Program.cs
@@ -8,13 +8,20 @@ public class MainClass
     public static void Main(string[] args)
     {
         List<int> data = ReadInput();
-        int result = data.Distinct().Last();
+        string result = CountZerosBeforeAndAfter(data);
         Console.WriteLine(result);
+    }
+
+    public static string CountZerosBeforeAndAfter(List<int> data)
+    {
+        var numbers = data.Select(x => x.ToString());
+        var arr = string.Join("", numbers).Split('1');
+        return $"Количество нулей перед единицей: {arr[0].Count()}, Количество нулей после единицы: {arr[1].Count()}";
     }
 
     public static List<int> ReadInput()
     {
         string input = Console.ReadLine();
-        return JsonSerializer.Deserialize<List<int>>(input);
+        return JsonSerializer.Deserialize<List<int>>(input.Trim());
     }
 }
