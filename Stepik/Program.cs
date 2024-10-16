2fd4bb449dc336aa71730360b88f2f4e797fba6d
G <xandir1993@gmail.com>
2024-10-16 12:27:28 +0300

    Напишите код, который находит количество серий из подряд идущих одинаковых чисел.
    Результат записать в переменную result.
    
    Например, в таком списке [1, 2, 2, 3, 3, 3, 4, 4, 4, 4] - 6 серий:

--- Stepik/Program.cs
+++ Stepik/Program.cs
@@ -8,26 +8,13 @@ public class MainClass
     public static void Main(string[] args)
     {
         List<int> data = ReadInput();
-        int result = FindLongestSeries(data);
+        int result = FindSeriesCount(data);
         Console.WriteLine(result);
     }
 
-    public static int FindLongestSeries(List<int> data)
+    public static int FindSeriesCount(List<int> data)
     {
-        int counter = 1;
-        var l = new List<int>();
-        for (int i = 0; i < data.Count - 1; i++)
-        {
-            if (data[i] < data[i+1])
-                counter++;
-            else
-            {
-                l.Add(counter);
-                counter = 1;
-            }
-            l.Add(counter);
-        }
-        return l.Max();
+        return Enumerable.Range(1, data.Count - 1).Where(x => data[x] == data[x - 1]).Count(); 
     }
 
     public static List<int> ReadInput()
